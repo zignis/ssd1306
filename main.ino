ba4fe0bf14007d358e7d08acded968fc4f4e0e5b
Coro <77036902+zignis@users.noreply.github.com>
2024-05-06 22:38:07 +0530

    Create main.ino

---
+++ main.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define OLED_RESET -1
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 alloc failed"));
+    for (;;);
+  }
+
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0, 0);
+  display.println("hello?");
+
+  display.display();
+}
+
+void loop() {}
